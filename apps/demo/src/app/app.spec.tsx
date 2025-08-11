7381a3e472bb848c63fc6ae1ff8992e099819cb0
Ira Hopkinson <ira_hopkinson@sil.org>
2025-08-12 10:42:03 +1200

    add hero component

--- apps/demo/src/app/app.spec.tsx
+++ apps/demo/src/app/app.spec.tsx
@@ -20,7 +20,7 @@ describe('App', () => {
       </BrowserRouter>
     );
     expect(
-      getAllByText(new RegExp('Welcome @acmer/demo', 'gi')).length > 0
+      getAllByText(new RegExp('Welcome to our demo', 'gi')).length > 0
     ).toBeTruthy();
   });
 });
---
+++ packages/ui/src/lib/ui.tsx
@@ -0,0 +1,11 @@
+import styles from './ui.module.css';
+
+export function AcmerUi() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to AcmerUi!</h1>
+    </div>
+  );
+}
+
+export default AcmerUi;
